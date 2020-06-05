fc1af366da338ceb70a190b104700f24b97859ac
KritikaRashid32 <kritikayadav950@gmail.com>
2020-06-05 21:37:41 +0530

    Final code uploaded

--- BattleShip.java
+++ BattleShip.java
@@ -1,12 +1,76 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class BattleShip {
 	Scanner scn = new Scanner(System.in);
 	final int size;
 	char[][] ocean;
 	boolean isManual;
-	Random rand;
+	Random rand = new Random();
 	final int ships;
+
+	private static class indent {
+
+		private static void printLegend() {
+			align(4, 0, "x: User Destroyed Ship!\n\n");
+			align(4, 0, "#: Computer's Destroyed Ship!\n\n");
+			align(4, 0, "@: User Saved Ship!\n\n");
+			align(4, 0, "!: Computer's Hidden Ship\n\n");
+		}
+
+		private static void align(int t, int l, String string) {
+
+			for (int i = 0; i < l; i++) {
+				System.out.println();
+			}
+			for (int i = 0; i < t; i++) {
+				System.out.print("\t");
+			}
+			System.out.print(string);
+
+		}
+
+		public static void separator(int value) {
+			System.out.println();
+			for (int i = 0; i < value; i++) {
+				System.out.print("-");
+			}
+			System.out.println();
+		}
+
+		public static void createDelay(String str) throws InterruptedException {
+			for (char ch : str.toCharArray()) {
+				System.out.print(ch);
+				TimeUnit.MILLISECONDS.sleep(60);
+			}
+			TimeUnit.MILLISECONDS.sleep(60);
+		}
+
+		private static void alignSlow(int t, int l, String string) throws InterruptedException {
+			for (int i = 0; i < l; i++)
+				System.out.println();
+
+			for (int i = 0; i < t; i++)
+				System.out.print("\t");
+
+			createDelay(string);
+		}
+
+		public static void leftpadding(int t) {
+			for (int i = 0; i < t; i++)
+				System.out.print("\t");
+		}
+
+		public static void instructions() throws InterruptedException {
+			align(4, 2, "1. Place your 5 ships in the Ocean.");
+			align(4, 2, "2. Then Computer will place its 5 ships.");
+			align(4, 2, "3. Then you both will be granted Chance one");
+			align(4, 2, "   by one to attack at desired position.");
+			align(4, 2, "4. Anyone who destroyed all of opponent's ship");
+			align(4, 2, "   will win. [$^_^$].\n\n");
+
+		}
+	}
 
 	// Default Constructor to initialize size of ocean and ships
 	public BattleShip() throws InterruptedException {
@@ -18,51 +82,23 @@ public class BattleShip {
 		this.ships = ships;
 		ocean = new char[this.size][this.size];
 		fill(ocean, '_');
-		separator(100);
-		align(4, 0, 0, "Status: Loading");
+		indent.separator(100);
+		indent.alignSlow(4, 0, "Status: Loading");
 		TimeUnit.MILLISECONDS.sleep(50);
 
 		for (int i = 0; i < 3; i++) {
 			System.out.print(".");
 			TimeUnit.SECONDS.sleep(1);
 		}
-		separator(100);
-		align(4, 1, 1, "Status: Completed!");
+		indent.separator(100);
+		indent.alignSlow(4, 1, "Status: Completed!\n");
 
-		separator(100);
+		indent.separator(100);
 		System.out.println();
 
-	}
+		indent.alignSlow(3, 0, "Welcome to the Battle Ship Game! [^_^]\n\n");
+		indent.alignSlow(2, 0, "I Present you the Ocean to Place your Ships! |-{*_*}-|\n\n");
 
-	private void align(int t, int l, int after, String string) throws InterruptedException {
-
-		for (int i = 0; i < l; i++) {
-			System.out.println();
-		}
-		for (int i = 0; i < t; i++) {
-			System.out.print("\t");
-		}
-		createDelay(string);
-
-		for (int i = 0; i < after; i++) {
-			System.out.println();
-		}
-	}
-
-	public void separator(int value) {
-		System.out.println();
-		for (int i = 0; i < value; i++) {
-			System.out.print("-");
-		}
-		System.out.println();
-	}
-
-	public void createDelay(String str) throws InterruptedException {
-		for (char ch : str.toCharArray()) {
-			System.out.print(ch);
-			TimeUnit.MILLISECONDS.sleep(70);
-		}
-		TimeUnit.MILLISECONDS.sleep(70);
 	}
 
 	public void fill(char[][] ocean, char val) throws InterruptedException {
@@ -71,189 +107,191 @@ public class BattleShip {
 		}
 	}
 
-	public void PrintOcean() throws InterruptedException {
+	public void PrintOcean(boolean show) throws InterruptedException {
 
-		align(3, 0, 2, "Welcome to the Battle Ship Game! [^_^]");
-		align(2, 0, 2, "I Present you the Ocean to Place your Ships! |-{*_*}-|");
-		align(4, "   ");
+		indent.align(4, 0, "   ");
 
 		for (int j = 0; j < ocean[0].length; j++)
 			System.out.print(j + " ");
 
 		System.out.println();
 		for (int row = 0; row < ocean.length; row++) {
-			leftpadding(4);
+			indent.leftpadding(4);
 			System.out.print(row + "| ");
 			for (int col = 0; col < ocean[row].length; col++) {
 
 				TimeUnit.MILLISECONDS.sleep(40);
+				if (ocean[row][col] == '1') {
+					System.out.print("@ ");
+					continue;
+				} else if (ocean[row][col] == '2') {
+					System.out.print((!show ? "_" : "!") + " ");
+					continue;
+				}
+
 				System.out.print(ocean[row][col] + " ");
 			}
 			System.out.print(row + "|");
 			System.out.println();
 		}
 		System.out.println();
-		align(4, "   ");
+		indent.align(4, 0, "   ");
 		for (int j = 0; j < ocean[0].length; j++)
 			System.out.print(j + " ");
 
 		System.out.println();
-		separator(100);
-	}
-
-	private void align(int t, int l, String string) throws InterruptedException {
-		for (int i = 0; i < l; i++)
-			System.out.println();
-
-		for (int i = 0; i < t; i++)
-			System.out.print("\t");
-
-		createDelay(string);
+		indent.separator(100);
 	}
 
 	public void Menu() throws InterruptedException {
-		align(4, 0, 2, "~~~~~MENU~~~~~\n");
-		align(4, 0, 1, "1. Start Game\n");
-		align(4, 0, 1, "2.How to Play\n");
-		align(4, 0, 1, "3.Exit Game\n");
+		indent.alignSlow(4, 0, "~~~~~MENU~~~~~\n\n\n");
+		indent.alignSlow(4, 0, "1. Start Game\n\n");
+		indent.alignSlow(4, 0, "2.How to Play\n\n");
+		indent.alignSlow(4, 0, "3.Exit Game\n\n");
 
 		while (true) {
-			separator(100);
-			align(4, 1, 0, "Enter your choice: ");
+			indent.separator(100);
+			indent.align(4, 1, "Enter your choice: ");
 			int choice = scn.nextInt();
 			if (choice == 1) {
+				Start();
 				break;
 			} else if (choice == 2) {
-				instructions();
+				indent.instructions();
 			} else {
 				break;
 			}
 		}
 	}
 
-	public void instructions() throws InterruptedException {
-		align(3, 2, "1. Place your 5 ships in the Ocean.");
-		align(3, 2, "2. Then Computer will place its 5 ships.");
-		align(3, 2, "3. Then you both will be granted Chance one");
-		align(3, 2, "   by one to attack at desired position.");
-		align(3, 2, "4. Anyone who destroyed all of opponent's ship");
-		align(3, 2, "   will win. [$^_^$].");
+	public int[] getXY(boolean isUser, boolean isManual) throws InterruptedException {
 
+		if (isUser && isManual) {
+			indent.align(4, 1, "Enter X and Y location : ");
+			return new int[] { scn.nextInt(), scn.nextInt() };
+		} else
+			return new int[] { rand.nextInt(10), rand.nextInt(10) };
 	}
 
-	public void align(int t, String s) {
-		for (int i = 0; i < t; i++) {
-			System.out.print("\t");
+	public void deploy(boolean isUser, Set<String> loc) throws InterruptedException {
+
+		if (isUser)
+			indent.alignSlow(4, 2, "Start placing your ships!!!\n\n");
+		else
+			indent.alignSlow(4, 2, "Computer placing it's ships\n\n");
+
+		for (int i = 0; i < 20;) {
+			int[] xy = getXY(isUser, false);
+			while (loc.contains(xy[0] + " " + xy[1])) {
+				indent.align(4, 1, "Cannot Place here... Try Again!!\n\n");
+				xy = getXY(isUser, false);
+				continue;
+			}
+			loc.add(xy[0] + " " + xy[1]);
+			if (isUser)
+				indent.align(4, 0, "" + (i + 1) + ". Your Ship Deployed at {" + xy[0] + ":" + xy[1] + "}\n\n");
+			else
+				indent.align(4, 0, "" + (i + 1) + ". Ship Deployed.\n\n");
+
+			this.ocean[xy[0]][xy[1]] = isUser ? '1' : '2';
+
+			i++;
 		}
-		System.out.print(s);
-	}
-
-	public void leftpadding(int t) {
-		for (int i = 0; i < t; i++)
-			System.out.print("\t");
-	}
-
-	/*
-	 * getXY: Method, params: isUSer, isManual isUSer && isManual: scn.nextInt()
-	 * otherwise: rand.nextInt(10) return: arr[2] = {X, Y}
-	 */
-
-	public void getXY(boolean isUser, boolean isManual) {
-
-		// check the above condition and return arr[2] (X, Y)
-	}
-
-	/*
-	 * 1. Deploy Ships: This method will deploy ships for User and Computer. params:
-	 * isUser, Loc(Set)
-	 */
-
-	public void deploy(boolean isUser, Set<String> loc) {
-
-		// 1. Print the message (User or Computer)
-
-		// 2. Place 20 ships using loop
-		// 2.1 calculate x and y: getXY(isUSer, false)
-		// 2.2 check for duplicate location
-		// 2.2.1 Print Message and continue
-		// 2.3 Corresponding Message
-		// 22.4 update the Ocean Matrix (1 for User) & (2 for Computer)
-		// 22.5 i++
-
-		// 3. Print Ocean
+		System.out.println();
+		System.out.println();
+		PrintOcean(false);
 
 	}
 
-	/*
-	 * KillShips: Method, params: isUser, Set(String) Loc print Messg (User: Choose
-	 * X and Y) otherwise Predict X and Y isUSer: takeInput X and Y: getXY(isUSer,
-	 * true) , Computer: Random: getXY(isUser, false) Kill at that position Return
-	 * the correspoinding Digit
-	 */
+	public int killShips(boolean isUSer, Set<String> loc) throws InterruptedException {
 
-	public void killShips(boolean isUSer, Set<String> loc) {
+		if (isUSer)
+			indent.align(4, 1, "Your Turn Choose Location to Fire {#__#} \n");
+		else
+			indent.align(4, 1, "Computer Firing {*__*} \n");
 
-		// 1. Print messg
+		int[] xy = getXY(isUSer, true);
 
-		// 2. X, Y = getXY(isUSer, true)
+		while (loc.contains(xy[0] + " " + xy[1])) {
+			indent.align(4, 1, "Cannot Fire Here!!!\n\n");
+			xy = getXY(isUSer, true);
+		}
 
-		// 3. Duplicate_loc
-		// 3.1 Loop till X and y are duplicates
-		// 3.1.1 Print messg (Cannot place. try again!!!)
-		// 3.1.2 X, Y = getXY(isUSer, true)
+		loc.add(xy[0] + " " + xy[1]);
 
-		// 4. Loc.add(X,Y)
+		if (this.ocean[xy[0]][xy[1]] == '1') {
+			if (isUSer)
+				indent.align(4, 1, "You Blown your own ship you Idiot [-__-]\n");
+			else
+				indent.align(4, 1, "Computer sunk one of your Ship {@__@}\n");
+			this.ocean[xy[0]][xy[1]] = 'x';
+			return 1;
+		} else if (this.ocean[xy[0]][xy[1]] == '2') {
+			if (isUSer)
+				indent.align(4, 1, "Boom!! You sunk Computer's Ship [^__^]\n");
+			else
+				indent.align(4, 1, "Computer sunk its own ship...[Noob] [-__-]\n");
+			this.ocean[xy[0]][xy[1]] = '#';
+			return 2;
+		}
 
-		// 5. ocean[X][Y] == ?
-		// 5.1 if value = 1
-		// 5.1.1 isUser: OwnShip, Computer: Boom!
-		// 5.1.2 ocean[X][Y] = x
-		// return 1
+		else {
+			System.out.println();
+			String res = isUSer ? "User" : "Computer";
 
-		// 5.2 if value = 2
-		// 5.2.1 isUSer: Boom!, Computer: Idiot
-		// 5.2.2 ocean[X][Y] = #
-		// 5.2.3 return 2
+			indent.alignSlow(4, 0, res + "  missed !!! \n");
+			return 0;
 
-		// 5.3 otherwise
-		// 5.3.1 Print (isUser: User Missed, Computer: Computer missed)
-		// 5.3.2 return 0
-
+		}
 	}
 
-	/*
-	 * 2. Start: Method , params - None It will Deploy ships for User & Computer.
-	 * This will keep track of the killed and left ships. At the end it will declare
-	 * the Winner Print the Ocean Print the Legends
-	 */
+	public void Start() throws InterruptedException {
+		Set<String> loc = new HashSet<>();
 
-	public void Start() {
+		deploy(true, loc);
 
-		// 1. Deploy user ships
+		deploy(false, loc);
 
-		// 2. Deploy Computer ships
+		int UserLeftShips = 5;
+		int CompLeftShips = 5;
+		Set<String> pos = new HashSet<>();
+		boolean isuser = true;
 
-		// 3. Declare UserLeftShips and CompLeftShips and Set(String) loc and isUSer =
-		// true
+		while (UserLeftShips > 0 && CompLeftShips > 0) {
 
-		// 4. Iterate till Game ends(UserLeftShips > 0 && CompLeftShips >0)
+			for (int i = 0; i < 2; i++) {
 
-		// 4.1 Loop: Which will run 2 times
-		// 4.1.1 KillShips: isUser, Set(String) loc
-		// 4.1.2 KillShips(Return type: int(1: User, 2:comp))
-		// 4.1.2.1 return == 1 (UserLeftShips--)
-		// 4.1.2.2 return == 2 (CompLeftShips--)
-		// 4.1.3 isUser = !isUser
+				int res = killShips(isuser, pos);
+				if (res == 1)
+					UserLeftShips--;
+				else if (res == 2)
+					CompLeftShips--;
+				isuser = !isuser;
+			}
 
-		// 4.2 Print Status: UserLeftShips Count and CompLeftShips Count
-		// 4.3 Print Ocean
+			UserLeftShips = UserLeftShips < 0 ? 0 : UserLeftShips;
+			CompLeftShips = CompLeftShips < 0 ? 0 : CompLeftShips;
+			indent.separator(100);
 
-		// 5. Decide Winner 
-		// 5.1 UserLeftShips <= 0: Computer won
-		// 5.2 CompLeftShips <= 0: User won
+			indent.align(4, 0, "Your Ship count: " + UserLeftShips + " Computer's Ship count: " + CompLeftShips);
 
-		// 6. Print Ocean
-		// 7. Print Legend
+			indent.separator(100);
+			PrintOcean(false);
+		}
+
+		if (UserLeftShips == 0 && CompLeftShips == 0)
+			indent.alignSlow(4, 2, "Its a Tie between Warriors :/\n\n");
+		else if (UserLeftShips == 0)
+			indent.alignSlow(4, 2, "Machine slayed Human [%__%]\n\n");
+		else
+			indent.alignSlow(4, 2, "Human defeated Machine (^__^)\n\n");
+
+		indent.separator(100);
+		System.out.println();
+		PrintOcean(true);
+		System.out.println();
+		indent.printLegend();
+		indent.separator(100);
 
 	}
 
